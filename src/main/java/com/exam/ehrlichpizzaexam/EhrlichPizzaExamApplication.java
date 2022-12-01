a1a630ed2cee2945a9a66fe4cdf33dc599f68c47
khianmontifar <khian.montifar@gmail.com>
2022-12-02 02:53:51 +0800

    Initial springboot codebase

---
+++ src/main/java/com/exam/ehrlichpizzaexam/EhrlichPizzaExamApplication.java
@@ -0,0 +1,13 @@
+package com.exam.ehrlichpizzaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhrlichPizzaExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhrlichPizzaExamApplication.class, args);
+	}
+
+}
